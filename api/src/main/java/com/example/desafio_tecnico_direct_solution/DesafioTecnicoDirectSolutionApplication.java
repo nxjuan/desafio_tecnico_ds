6984765567a6d36374d90ca2e5628efb8115343a
Juan Nogueira <juannogueiraxavier@gmail.com>
2025-08-25 19:50:20 -0300

    first commit - starting api

---
+++ api/src/main/java/com/example/desafio_tecnico_direct_solution/DesafioTecnicoDirectSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_tecnico_direct_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoDirectSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoDirectSolutionApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/example/desafio_tecnico_direct_solution/DesafioTecnicoDirectSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_tecnico_direct_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoDirectSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
