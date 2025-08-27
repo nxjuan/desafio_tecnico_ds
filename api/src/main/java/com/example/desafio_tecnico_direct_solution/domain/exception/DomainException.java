f7f28a1516a3c6d80a10eaacbe1d12dae6255d35
Juan Nogueira <juannogueiraxavier@gmail.com>
2025-08-27 18:49:56 -0300

    domain: DomainException

---
+++ api/src/main/java/com/example/desafio_tecnico_direct_solution/domain/exception/DomainException.java
@@ -0,0 +1,5 @@
+package com.example.desafio_tecnico_direct_solution.domain.exception;
+
+public class DomainException extends RuntimeException{
+    public DomainException(String message) { super(message); }
+}
